9a9999e9bde5dad804d2bf7621e3d6833d7dd249
SC1314520 <g0710126@hchs.hc.edu.tw>
2025-05-28 13:30:35 +0800

    Top component

---
+++ src/pages/Login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Login'
+}
+</script>
+
+<style scoped>
+    .main{
+        display: flex;
+        width: 100vw;
+        height: calc(100vh - 60px);
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Index from '../pages/Index/Index.vue'
-
+import Login from '../pages/Login/Login.vue'
 Vue.use(VueRouter)
 const router = new VueRouter({
     routes:[
         {
             path:'/index',
             component:Index
+        },
+        {
+            path:'/login',
+            component:Login
         },
         {
             path:'/',

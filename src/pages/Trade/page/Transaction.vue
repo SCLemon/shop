052323bc6cec2d2d94df6600f9a25fd20866edc5
SC1314520 <g0710126@hchs.hc.edu.tw>
2025-05-30 21:12:22 +0800

    s

--- src/pages/Trade/page/Transaction.vue
+++ src/pages/Trade/page/Transaction.vue
@@ -178,108 +178,108 @@
   </script>
   
   <style scoped>
-      .main{
-          width: 100vw;
-          height: calc(100vh - 206px);
-          overflow-y: scroll;
-      }
-      .list{
-          width: 95vw;
-          height: calc((100vh - 216px) / 4);
-          margin: 0 auto;
-          display: flex;
-          justify-content: space-evenly;
-          align-items: center;
-          border-bottom: 1px solid rgba(210,210,210,0.3);
-          position: relative;
-      }
-      .label{
-        height: 22.5px;
-        line-height: 22.5px;
-        position: absolute;
-        top:0;
-        left:0;
-        font-size: 12px;
-        padding-left: 7px;
-        padding-right: 7px;
-        border-radius: 0 0 7px 0;
-        background: chocolate;
-        color: white;
-        z-index: 101;
-      }
-      .list_img{
-          width: 120px;
-          height: 120px;
-      }
-      .list_content{
-          width: calc(95vw - 150px);
-          height: 120px;
-          margin-left: 10px;
-          position: relative;
-      }
-      .img_box{
-          width: 120px;
-          height: 120px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-      }
-      .img_box img{
-          width: 100%;
-          height: 100%;
-          object-fit: contain;
-      }
-      .list_title{
-          width: 100%;
-          line-height: 35px;
-          font-weight: bolder;
-          display: -webkit-box;
-          -webkit-line-clamp: 1;
-          -webkit-box-orient: vertical;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          box-sizing: border-box;
-      }
-      .list_detail{
-          font-size: 12px;
-          color: gray;
-          line-height: 1.5;
-          display: -webkit-box;
-          -webkit-line-clamp: 2;
-          -webkit-box-orient: vertical;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          box-sizing: border-box;
-      }
-      .list_bottom{
-          width: 100%;
-          position: absolute;
-          bottom: 0px;
-          height: 40px;
-          display: flex;
-          align-items: center;
-      }
-      .list_price{
-          font-size: 14px;
-      }
-      .list_bottom_right{
-          margin-left: auto;
-      }
-      .list_bottom_btn{
-          margin-left: 20px;
-      }
-      .trash{
-          color: red;
-          margin-left: 5px;
-      }
-      .trash:hover{
-          cursor: pointer;
-      }
-      .mobile_right_quantity{
-          display: none;
-      }
-      .empty{
-        width: 100vw;
-        height: calc(100vh - 206px);
-      }
+        .main{
+            width: 100vw;
+            height: calc(100vh - 206px);
+            overflow-y: scroll;
+        }
+        .list{
+            width: 95vw;
+            height: calc((100vh - 216px) / 4);
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+            border-bottom: 1px solid rgba(210,210,210,0.3);
+            position: relative;
+        }
+        .label{
+            height: 22.5px;
+            line-height: 22.5px;
+            position: absolute;
+            top:0;
+            left:0;
+            font-size: 12px;
+            padding-left: 7px;
+            padding-right: 7px;
+            border-radius: 0 0 7px 0;
+            background: chocolate;
+            color: white;
+            z-index: 101;
+        }
+        .list_img{
+            width: 120px;
+            height: 120px;
+        }
+        .list_content{
+            width: calc(95vw - 150px);
+            height: 120px;
+            margin-left: 10px;
+            position: relative;
+        }
+        .img_box{
+            width: 120px;
+            height: 120px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .img_box img{
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+        }
+        .list_title{
+            width: 100%;
+            line-height: 35px;
+            font-weight: bolder;
+            display: -webkit-box;
+            -webkit-line-clamp: 1;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            box-sizing: border-box;
+        }
+        .list_detail{
+            font-size: 12px;
+            color: gray;
+            line-height: 1.5;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            box-sizing: border-box;
+        }
+        .list_bottom{
+            width: 100%;
+            position: absolute;
+            bottom: 0px;
+            height: 40px;
+            display: flex;
+            align-items: center;
+        }
+        .list_price{
+            font-size: 14px;
+        }
+        .list_bottom_right{
+            margin-left: auto;
+        }
+        .list_bottom_btn{
+            margin-left: 20px;
+        }
+        .trash{
+            color: red;
+            margin-left: 5px;
+        }
+        .trash:hover{
+            cursor: pointer;
+        }
+        .mobile_right_quantity{
+            display: none;
+        }
+        .empty{
+            width: 100vw;
+            height: calc(100vh - 206px);
+        }
   </style>

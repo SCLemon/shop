c4da7573049ba8488188e2aaa0330954a252f4e1
SC1314520 <g0710126@hchs.hc.edu.tw>
2025-05-31 14:19:13 +0800

    pwa

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
     lintOnSave: false,
     publicPath: './',
     devServer:{
-        https:false,
+        https:true,
         proxy: {
             '/api': {
                 target: 'http://127.0.0.1:3007',
